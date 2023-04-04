80c9dd602a9af131ec616a18b420f0911dbca0c5
piceli <39059455+piceli@users.noreply.github.com>
2023-04-04 18:48:05 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,5 +16,5 @@ pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_mac
 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 
-
+streamlit.dataframe(my_fruits_list)
 
